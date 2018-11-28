421692fcbc3e6f743696420b55aca02b88181432
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-28 16:44:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,6 +109,20 @@ client.on('guildMemberAdd', member => {
       channel.sendEmbed(embed);
     });
     
+client.on('message', msg => {
+
+    if (msg.content == '=join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("517332538163855364").join();
+    });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
